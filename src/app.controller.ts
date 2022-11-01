9b6840f01529917df5a88cd65ae1bd298b294d61
AyberkCakar <ayberkcakar35@gmail.com>
2022-11-01 22:57:37 +0300

    feat: added redis queue structure example

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,9 +7,11 @@ import {
   Get,
   Inject,
   UseInterceptors,
-} from '@nestjs/common';
-import { Cache } from 'cache-manager';
-import { getCats, getDogs } from './utils';
+  Query,
+} from "@nestjs/common";
+import { Cache } from "cache-manager";
+import { getCats, getDogs } from "./utils";
+import { MessageProducer } from "./message.producer";
 
 @UseInterceptors(CacheInterceptor)
 @Controller()
@@ -17,36 +19,43 @@ export class AppController {
   constructor(
     @Inject(CACHE_MANAGER)
     private cacheManager: Cache,
+    private messageProducer: MessageProducer
   ) {}
 
-  @CacheKey('all-dogs')
-  @Get('dogs')
+  @CacheKey("all-dogs")
+  @Get("dogs")
   getDogs() {
     return getDogs();
   }
 
-  @Get('dogs-2')
+  @Get("dogs-2")
   async getDogs2() {
-    const cachedDogs = await this.cacheManager.get('all-dogs2');
+    const cachedDogs = await this.cacheManager.get("all-dogs2");
     if (cachedDogs) return cachedDogs;
 
     const dogs = await getDogs();
-    this.cacheManager.set('all-dogs2', dogs, 10);
+    this.cacheManager.set("all-dogs2", dogs, 10);
 
     return dogs;
   }
 
-  @Get('cats')
-  @CacheKey('all-cats')
+  @Get("cats")
+  @CacheKey("all-cats")
   async getCats() {
     const cats = await getCats();
 
     return cats;
   }
 
-  @Delete('dogs')
+  @Delete("dogs")
   async deleteCats() {
-    this.cacheManager.del('all-dogs2');
-    return 'ok';
+    this.cacheManager.del("all-dogs2");
+    return "ok";
+  }
+
+  @Get("send-message")
+  async sendMessage(@Query("message") message: string) {
+    this.messageProducer.sendMessage(message);
+    return message;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import * as redisStore from 'cache-manager-ioredis';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
+import { BullModule } from '@nestjs/bull';
+import { MessageProducer } from './message.producer';
+import { MessageConsumer } from './message.consumer';
 
 @Module({
   imports: [
@@ -16,9 +19,19 @@ import configuration from './config/configuration';
       port: Number(configuration().redis.port),
       password: String(configuration().redis.password),
     }),
+    BullModule.forRoot({
+      redis: {
+        host: String(configuration().redis.host),
+        port: Number(configuration().redis.port),
+        password: String(configuration().redis.password),
+      }
+    }),
+    BullModule.registerQueue({
+      name: 'message-queue'
+    })
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MessageProducer, MessageConsumer],
 })
 export class AppModule {
   constructor(public configService: ConfigService) {}
